a4cdb87fffdc91470733ccba0bec8cd33aa60c68
Cortesao <n.cortesao.dias@gmail.com>
2018-07-18 16:11:12 +0100

    InputRevers Splited Into Requisites // Extra tool sha3_keccak module added // Example ABI added

--- InputReverser.py
+++ InputReverser.py
@@ -3,9 +3,23 @@
 #Date: 16/07/2018
 
 #inported methods:
-import  extraTools 
+import extraTools
+
+#Receive Inputdata  // Can come from file in the future but for now will be directly applied
+Inputdata=""
+
+#Read ABI
+
+
 #The first 4 bytes represent the function (4 bytes = 8 chars)
 
-Nuno="nuno"
 
-print(extraTools.stringToHex(Nuno))
+#Call File that will convert all the ABI functions into the keccak4byte model: Receive the ABI File and return list
+
+#Verify the presence of the Function from Input Data into the AbiConvertedList
+
+#If not present end script and error list
+
+#If present split InputData string into the variable numbers
+
+#Using the ABI variable list reconvert the data inside the InputData String
