fa829cadb2659eb77910bd4bc2fa8e8f7e89aa22
Cortesao <n.cortesao.dias@gmail.com>
2018-07-19 17:10:15 +0100

    Added: Abi name Arg retriever

--- abiModules.py
+++ abiModules.py
@@ -11,4 +11,10 @@ def AbijsonReader(s):
         data = json.load(f)
     #Data will have a list of all the arguments inside the ABIJSON
     #Arguments are composed by dictionaries where type define if its a function, an event, a fallback or a constructor
-    return data
+    return data #list will be returned
+
+def argumentNameRetriever(dic): #receives an ABI argument -> should be a dictionary
+    if dic["type"].encode('ascii','ignore')=="function":
+        nameFunction=dic["name"].encode('ascii','ignore') #information from the ABI comes in unicode so a Ascii modification is required
+        #print(nameFunction)
+        return(nameFunction)
